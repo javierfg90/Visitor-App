1a641c58ad953d9c853103a12ba39cad8aad3568
Javier Fernandez <57125060+javierfg90@users.noreply.github.com>
2020-05-25 18:37:59 -0400

    Add files via upload

---
+++ Visitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ToDoApII.Models
+{
+    public class Visitor
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Arrival => DateTime.Now;
+        public bool Male { get; set; }
+        public int Age { get; set; }
+
+        public double Price { get; set; }
+        public int Nights { get; set; }
+        public double Amount => Price * Nights;
+        public bool Adult => Age >= 18;
+        public string Country { get; set; }
+    }
+}
